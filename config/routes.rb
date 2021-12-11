43a1e8eed85fdd47d03142b1e51edebd9754ed91
masahiro.hataoka <acxdl48788@yahoo.co.jp>
2021-12-11 06:27:00 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
 
   resources :tasks
-  resources :users, only: [:index,:show,:new,:create]
+  resources :users, only: [:new,:create]
 end
